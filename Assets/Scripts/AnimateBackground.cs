f142324d94695dae0065b95b9733c012c31d4c5a
Slynchy <sam.lynch2@hotmail.co.uk>
2017-03-04 18:17:49 +0000

    Added main menu

---
+++ Assets/Scripts/AnimateBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class AnimateBackground : MonoBehaviour {
+
+    public float Speed = 1.0f;
+    private ScreenOverlay overlay;
+    private float counter = 0.0f;
+    private int currIndex = 0;
+
+    public Texture2D[] images;
+
+    // Use this for initialization
+    void Start () {
+        overlay = this.GetComponent<ScreenOverlay>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter += Speed * Time.deltaTime;
+		
+        if(counter >= 1.0f)
+        {
+            counter = 0.0f;
+            currIndex++;
+            if (currIndex >= images.Length) currIndex = 0;
+            overlay.texture = images[currIndex];
+        }
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    bool fired = false;
+    public GameObject ninja;
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fired == false) return;
+
+        ninja.GetComponent<Animator>().SetFloat("velocityX", Mathf.Abs(3.0f));
+        ninja.transform.Translate(3.0f * Time.deltaTime, 0, 0);
+
+        if (ninja.transform.position.x >= 40.0f)
+        {
+            //done
+        }
+    }
+
+    public void FireOnClick()
+    {
+        fired = true;
+    }
+}
