f20b037b549d0dc32e12044534810ca0492da964
VasilStamatov <vasil_0000@abv.bg>
2017-03-04 22:11:33 +0000

    Added a timer for the time passed UI and prefabbed all the new ui stuff with the camera

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+		public Text text;
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+				int minutes = (int)(Time.time / 60.0f);
+				int seconds = (int)Mathf.Repeat(Time.time, 60.0f);
+				int millis = (int)Mathf.Repeat(Time.time * 1000.0f, 1000.0f);
+				text.text = minutes.ToString().PadLeft(2, '0') + ":" + seconds.ToString().PadLeft(2, '0') + "." + millis.ToString().PadLeft(3, '0');
+		}
+}
