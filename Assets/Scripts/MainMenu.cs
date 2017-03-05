64f2c72bbf58d41c90f410fa16e724e9ba74bcdd
Slynchy <sam.lynch2@hotmail.co.uk>
2017-03-05 22:07:54 +0000

    Almost finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,30 +2,31 @@
 
 public class MainMenu : MonoBehaviour
 {
-		/** The ninja prefab */
-		public GameObject ninja;
+    /** The ninja prefab */
+    public GameObject ninja;
 
-		/** has the game been fired */
-		private bool fired = false;
-		/** a timer */
-		private float timer = 0.0f;
+    /** has the game been fired */
+    private bool fired = false;
+    /** a timer */
+    private float timer = 0.0f;
 
-		// Update is called once per frame
-		void Update()
-		{
-				if (fired == false) return;
+    // Update is called once per frame
+    void Update()
+    {
+        if (fired == false) return;
 
-				ninja.GetComponent<Animator>().SetFloat("velocityX", Mathf.Abs(3.0f));
-				ninja.transform.Translate(3.0f * Time.deltaTime, 0, 0);
+        ninja.GetComponent<Animator>().SetFloat("velocityX", Mathf.Abs(3.0f));
+        ninja.transform.Translate(3.0f * Time.deltaTime, 0, 0);
 
-				if (ninja.transform.position.x >= 40.0f)
-				{
-						//done
-				}
-		}
+        if (ninja.transform.position.x >= 40.0f)
+        {
+            //done
+            GameObject.Find("Canvas").GetComponent<UIManager>().OnLevelSelector();
+        }
+    }
 
-		public void FireOnClick()
-		{
-				fired = true;
-		}
+    public void FireOnClick()
+    {
+        fired = true;
+    }
 }
